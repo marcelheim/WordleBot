ebec8957b72965774186cc795e2acb470f29c4d2
Marcel Heim <40496374+marcelheim@users.noreply.github.com>
2022-05-31 17:59:37 +0200

    Add download link

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -31,6 +31,9 @@ const logout = () => {
           <li>
             <Link to="" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</Link>
           </li>
+          <li>
+            <a href="https://github.com/marcelheim/WordleBot" target="tab" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Download</a>
+          </li>
           {#if $auth}
             <li>
               <Link to="management" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Management</Link>
